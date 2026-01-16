8c20896a056b47bd97c39bf0df3f5782c9f0de1c
GersonESS <gebhsantos@gmail.com>
2026-01-16 20:34:40 -0300

    s

---
+++ form/form/src/App.tsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+      <h1>Vite + React</h1>
+      
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
