226ee93b0d2931b9a40c3269e9242e9d8bb2b976
GersonESS <gebhsantos@gmail.com>
2026-01-16 22:50:53 -0300

    a

---
+++ 00form/form/src/App.tsx
@@ -0,0 +1,18 @@
+// import "./App.css";
+// import { RHFYouTubeForm } from "./components/RHFYouTubeForm";
+ import { YouTubeForm } from "./components/YouTubeForm.tsx";
+// import { ZodYouTubeForm } from "./components/ZodYouTubeForm";
+//import { LoginForm } from "./components/MuiLoginForm";
+
+function App() {
+  return (
+    <div>
+      {/* <RHFYouTubeForm /> */}
+      <YouTubeForm />
+      {/* <ZodYouTubeForm /> */}
+      {/* <LoginForm /> */}
+    </div>
+  );
+}
+
+export default App;
