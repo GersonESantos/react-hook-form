ca4ad56364ebc8c86628e5ebc156d297756dd565
GersonESS <gebhsantos@gmail.com>
2026-01-16 22:15:59 -0300

    Create YouTubeForm.jsx

---
+++ 00form/form/src/components/YouTubeForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const YouTubeForm = () => {
+  return (
+    <div>
+      <h1>Yup YouTube Form</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" {...register("username")} />
+          <p className="error">{errors.username?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" {...register("email")} />
+          <p className="error">{errors.email?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input type="text" id="channel" {...register("channel")} />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default YouTubeForm
